9fab3cf9b08e8984b432ef4f359285ada0a472d2
Rohan Chilukuri <rohan.chilukuri@outlook.com>
2024-09-07 20:40:46 -0700

    i am not arron

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,8 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify
+import json
 
 app = Flask(__name__)
 
+deaths = "deaths.json"
 
+@app.route('/dead', methods=['POST'])
+def dead():
+    with open(deaths, 'r') as f:
+        data = json.load(f)
+    
+    if "deaths" in data:
+        data["deaths"] += 1
+    else:
+        data["deaths"] = 1
+    
+    with open(deaths, 'w') as f:
+        json.dump(data, f)
+
+    return jsonify({"deaths": data["deaths"]})
+
+
+@app.route('/get_deaths', methods=['GET'])
+def get_deaths():
+    with open(deaths, 'r') as f:
+            data = json.load(f)
+
+    return jsonify({"deaths": data["deaths"]})
 
 if __name__ == '__main__':
     app.run(debug=True)
